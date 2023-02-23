14b5d757e78dca30d50e1d0228010fb6f6db81bb
Mika <mikberza@mail.ru>
2023-02-24 00:19:36 +0300

    6_3_pilaf_page Выполнено в проекте: -очистил проект от ненужных файлов; -добавил папки (img, components); -разбил проект на 6 компонентов; -добавил собственную стилизацию; -добавил стилизацию из bootstrap; -написал функционал для расчета нужного кол-ва продуктов в зависимости от кол-ва порций блюда;
    Done in the project:
    -cleared the project of unnecessary files;
    -added folders (img, components);
    -split the project into 6 components;
    -added my own styling;
    -added styling from bootstrap;
    -wrote a functional for calculating the required number of products depending on the number of portions of the dish;

---
+++ 6_3_pilaf_page/src/components/Heading.js
@@ -0,0 +1,14 @@
+//
+//
+//
+function Heading() {
+  return (
+    <div className='container-xxl'>
+      <h2 className='card-title'>
+        <a name='high-speed-click'></a>Плов - расчет ингридиентов
+      </h2>
+    </div>
+  );
+}
+
+export default Heading;
